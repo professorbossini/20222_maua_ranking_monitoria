6190eab3d8274e67c2c20f34d1ae154be5ce0b88
Rodrigo Bossini <professorbossini@gmail.com>
2022-04-17 20:32:03 -0300

    Pré Programação
    Endpoint que dá acesso ao ranking implementado. Ordenação por pontuação. Dados extraidos diretamente da planilha alimentada pelos tutores"

--- index.js
+++ index.js
@@ -1,21 +1,39 @@
 require('dotenv').config()
 const axios = require('axios')
 const express = require('express')
+const titleCase = require('title-case')
+const _ = require('underscore')
 
-const {GOOGLE_KEY, GOOGLE_SHEET_PRE_PROGRAMACAO_ID, GOOGLE_BASE_URL_SHEET} = process.env
+const {GOOGLE_KEY, GOOGLE_SHEET_NAME, GOOGLE_SHEET_PRE_PROGRAMACAO_ID, GOOGLE_BASE_URL_SHEET, PORT} = process.env
 
 
-// const testeAxios = () => {
-//     const url = `${GOOGLE_BASE_URL_SHEET}${GOOGLE_SHEET_PRE_PROGRAMACAO_ID}/values/Sheet1?key=${GOOGLE_KEY}`
-//     console.log(url)
-//     console.log(url)
-//     axios.get(url)
-//         .then(response => {
-//             console.log(response.data)
-//         })
-//         .catch(error => {
-//             console.log(JSON.stringify(error))
-//         })
-// }
+const app = express()
+app.use(express.json())
 
-// testeAxios()
+app.get('/pre_prog/ranking', async (req, res) => {
+    try{
+        const url = `${GOOGLE_BASE_URL_SHEET}${GOOGLE_SHEET_PRE_PROGRAMACAO_ID}/values/${GOOGLE_SHEET_NAME}?key=${GOOGLE_KEY}`
+        const dados = await axios.get(url)
+        const ranking = dados.data.values.slice(1).map(dado => {
+            return {
+                ra: dado[0],
+                nome: titleCase.titleCase((dado[1])),
+                ies: titleCase.titleCase((dado[2])),
+                pontos: dado.slice(3).reduce((acc, curr) => acc + Number(curr), 0)
+            }
+        })
+        res.json(_.sortBy(ranking, 'pontos').reverse())
+    }
+    catch(error){
+        console.log(error)
+        res.send('Tente novamente mais tarde')
+    }
+})
+
+app.get('/tech_in_oracle/ranking', (req, res) => {
+    res.end()
+})
+
+app.listen(PORT, () => {
+    console.log(`Up and running on port ${PORT}`)
+})
