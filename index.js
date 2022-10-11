ae17e52ebb8924c754d2422af26437b09ccd3ac7
Rodrigo Bossini <professorbossini@gmail.com>
2022-10-11 12:12:07 -0300

    vlibras endpoint added

--- index.js
+++ index.js
@@ -42,6 +42,22 @@ app.get('/', (req, res) => {
 })
 
 
+
+app.get('/vlibras', (req, res) => {
+  const widget = `div vw class="enabled">
+    <div vw-access-button class="active"></div>
+    <div vw-plugin-wrapper>
+      <div class="vw-plugin-top-wrapper"></div>
+    </div>
+  </div>
+  <script src="https://vlibras.gov.br/app/vlibras-plugin.js"></script>
+  <script>
+    new window.VLibras.Widget('https://vlibras.gov.br/app');
+  </script>`
+  res.send(widget)
+})
+
+
 app.listen(PORT || 3000, () => {
     console.log(`Up and running on port ${PORT}`)
 })
